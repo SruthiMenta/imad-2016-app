fb36f76aab9b97fed99171372856f2a74d089fac
SruthiMenta <deepsruthi@gmail.com>
2016-09-25 15:18:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articlename', function (req,res) {
-    var articlenames=req.params.articlenames;
+    var articlename=req.params.articlenames;
      res.send(createtemplate(articles[articlename]));
      
 });
