27dab516550d1f6df7b305344a162bfa7f8dc9fc
SruthiMenta <deepsruthi@gmail.com>
2016-09-25 15:48:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img=document.getELementById('madi');
+var marginleft=0;
+function moveright(){
+    marginleft+=5;
+    img.style.marginLeft=marginleft +'px';
+}
+img.onclick=function(){
+    var interval=setinterval(moveright,50);
+};
