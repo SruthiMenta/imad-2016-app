6ae2dc0e2e929bbcc03279830713db99cfdaedc4
SruthiMenta <deepsruthi@gmail.com>
2016-09-30 22:25:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,22 @@ img.onclick= function() {
 var btn=document.getElementById('counter');
 var counter=0;
 btn.onclick= function(){
+    //create an object
+    var request=new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onReadyStateChanged=function() {
+        if(request.ReadyState===XMLHttpRequest.DONE)
+        {
+            //take some aciton
+            if(request.status==200)
+            {
+                var counter=request.responseText;
+                var span=request.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
