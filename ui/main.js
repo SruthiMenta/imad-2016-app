a09e8a0652690680d26a95fe68a6985adedf1e21
SruthiMenta <deepsruthi@gmail.com>
2016-09-30 22:08:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,7 @@ img.onclick= function() {
     var interval=setInterval(moveRight,50);
 };
 var btn=document.getElementById('counter');
+var counter=0;
 btn.onclick= function(){
     counter=counter+1;
     var span=document.getElementById('count');
